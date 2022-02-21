fda13347ba8fe848371bbc3ffe95e05db0f8d9d1
Andrea Opreni <andrea.opreni@outlook.com>
2022-02-21 15:53:47 +0100

    added library

--- src/MORFEInvariantManifold.jl
+++ src/MORFEInvariantManifold.jl
@@ -8,6 +8,7 @@ module MORFEInvariantManifold
   using FileIO
   using LinearAlgebra
   using Arpack
+  using DelimitedFiles
 
   export MORFE_add_material, MORFE_list_materials, MORFE_delete_material, MORFE_load_material
   export MORFE_mech_autonomous, MORFE_mech_nonautonomous

16207815164d1a7fbc15423f4ffefdee91227c3a
Andrea Opreni <andrea.opreni@outlook.com>
2022-04-12 08:51:03 +0200

    Update MORFEInvariantManifold.jl

--- src/MORFEInvariantManifold.jl
+++ src/MORFEInvariantManifold.jl
@@ -14,7 +14,7 @@ module MORFEInvariantManifold
 
   export MORFE_add_material, MORFE_list_materials, MORFE_delete_material, MORFE_load_material
   export MORFE_mech_autonomous, MORFE_mech_nonautonomous
-  export MORFE_backbone_modal
+  export MORFE_integrate_rdyn_backbone, MORFE_compute_backbone_modal
 
   # collection of parametrisation calls
   include("dpim_calls.jl")
